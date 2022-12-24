40e17a5d0e475a1fc49121dfb0d295aa30ad2fa6
기면증 <70426440+hyjoong@users.noreply.github.com>
2022-12-24 23:45:48 +0900

    feat: main page 폴더구조 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import { BaseLayoutStyled } from "@components/commons/BaseLayout/style";
-import Button from "@components/commons/Button";
-import React from "react";
+import BaseLayout from "@components/commons/BaseLayout";
+import Landing from "@components/main/Landing";
 
 const Home = () => {
   return (
-    <BaseLayoutStyled>
-      <p>Quiz Game</p>
-      <Button>퀴즈 풀기</Button>
-    </BaseLayoutStyled>
+    <BaseLayout>
+      <Landing />
+    </BaseLayout>
   );
 };
 
