4ad9b2d2d04e3948fd68da14695471dd6a4621af
기면증 <70426440+hyjoong@users.noreply.github.com>
2022-12-24 18:31:24 +0900

    feat: category & difficulty constants

---
+++ constants/select.ts
@@ -0,0 +1,14 @@
+export const QUIZ_CATEGORY = [
+  { title: "category", content: "ALL", value: null },
+  { title: "category", content: "Music", value: 12 },
+  { title: "category", content: "Board Games", value: 16 },
+  { title: "category", content: "Mathematics", value: 19 },
+  { title: "category", content: "Sports", value: 21 },
+];
+
+export const QUIZ_DIFFICULTY = [
+  { title: "difficulty", content: "ALL", value: null },
+  { title: "difficulty", content: "EASY", value: "easy" },
+  { title: "difficulty", content: "MEDIUM", value: "medium" },
+  { title: "difficulty", content: "HARD", value: "hard" },
+];
