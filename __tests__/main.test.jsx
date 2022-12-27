524533510d432b9453a9357a0265cc8826e9678f
기면증 <70426440+hyjoong@users.noreply.github.com>
2022-12-27 09:17:56 +0900

    test: main page next-router-mock 적용

--- __tests__/main.test.jsx
+++ __tests__/main.test.jsx
@@ -1,12 +1,20 @@
 import "@testing-library/jest-dom";
 import Home from "../pages/index";
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+    };
+  },
+}));
 
 describe("<Home />", () => {
-  it("render main page", () => {
-    const { container } = render(<Home />);
+  const { container } = render(<Home />);
 
+  it("render main page", () => {
     expect(container).toHaveTextContent("Quiz Game");
     expect(container).toHaveTextContent("퀴즈 풀기");
   });
