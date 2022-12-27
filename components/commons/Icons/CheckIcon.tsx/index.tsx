73beb8c659b438e5cdeb7a4c455421270504d466
기면증 <70426440+hyjoong@users.noreply.github.com>
2022-12-28 07:58:34 +0900

    feat: svg 파일들 속성 camelCase로 수정

--- components/commons/Icons/CheckIcon.tsx/index.tsx
+++ components/commons/Icons/CheckIcon.tsx/index.tsx
@@ -6,16 +6,16 @@ const CheckIcon = () => {
       <path
         d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
         stroke="#00C896"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M16 10L10.5 15L8 12.7273"
         stroke="#00C896"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- components/commons/Icons/XIcon.tsx/index.tsx
+++ components/commons/Icons/XIcon.tsx/index.tsx
@@ -6,9 +6,9 @@ const XIcon = () => {
       <path
         d="M14 8L8 14M8 8L14 14M21 11C21 16.5228 16.5228 21 11 21C5.47715 21 1 16.5228 1 11C1 5.47715 5.47715 1 11 1C16.5228 1 21 5.47715 21 11Z"
         stroke="#F04438"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
