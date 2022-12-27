a8e84cab5edf7274e5b87bb0cb6fa07ea28efb08
기면증 <70426440+hyjoong@users.noreply.github.com>
2022-12-27 20:33:04 +0900

    feat: app file adjust initMock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/reset.css";
 import type { AppProps } from "next/app";
+import initMockAPI from "@mocks/index";
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (process.env.NODE_ENV === "development") {
+    initMockAPI();
+  }
+
   return <Component {...pageProps} />;
 }
