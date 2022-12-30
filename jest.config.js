43dc2b74dccd2248fa0f3f334d5c37ffcb77d285
기면증 <70426440+hyjoong@users.noreply.github.com>
2022-12-30 17:24:57 +0900

    feat: jest setting

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,15 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
     "^@components/(.*)$": "<rootDir>/components/$1",
     "^@pages/(.*)$": "<rootDir>/pages/$1",
+    "^@hooks/(.*)$": "<rootDir>/hooks/$1",
+    "^@type/(.*)$": "<rootDir>/type/$1",
+    "^@lib/(.*)$": "<rootDir>/lib/$1",
+    "^@constants/(.*)$": "<rootDir>/constants/$1",
+    "^@utils/(.*)$": "<rootDir>/utils/$1",
+    "^@store/(.*)$": "<rootDir>/store/$1",
+    "^@mocks/(.*)$": "<rootDir>/mocks/$1",
   },
   testEnvironment: "jest-environment-jsdom",
 };
