c75e99cd136a986d3b9029b756a305dcef2fea7d
기면증 <70426440+hyjoong@users.noreply.github.com>
2022-12-30 17:22:06 +0900

    delete: setting store 제거

--- store/setting/type.ts
+++
@@ -1,8 +0,0 @@
-import { OptionProps } from "@type/option";
-
-export interface Setting {
-  category: OptionProps["value"];
-  difficulty: OptionProps["value"];
-  setCategory: (optionValue: OptionProps["value"]) => void;
-  setDifficulty: (optionValue: OptionProps["value"]) => void;
-}
