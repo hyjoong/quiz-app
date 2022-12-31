c0f84a21f78549d15afa6d49e9e01b2a8dae6ce7
기면증 <70426440+hyjoong@users.noreply.github.com>
2023-01-01 00:24:18 +0900

    test: main page test

--- __tests__/main.test.jsx
+++ __tests__/main.test.jsx
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
-import Home from "../pages/index";
-
-import { render, screen } from "@testing-library/react";
+import Home from "@pages/index";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { QUIZ_CATEGORY, QUIZ_DIFFICULTY, QUIZ_NUMBER } from "@constants/select";
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -12,10 +12,39 @@ jest.mock("next/router", () => ({
 }));
 
 describe("<Home />", () => {
-  const { container } = render(<Home />);
+  render(<Home />);
+  const title = screen.getByText("Quiz Game");
+  const categorySelector = screen.getByText("카테고리");
+  const difficultySelector = screen.getByText("난이도");
+  const countSelector = screen.getByText("문제수");
+  const quizButton = screen.getByRole("button");
 
-  it("render main page", () => {
-    expect(container).toHaveTextContent("Quiz Game");
-    expect(container).toHaveTextContent("퀴즈 풀기");
+  it("main page render test", () => {
+    expect(title).toBeInTheDocument();
+    expect(categorySelector).toBeInTheDocument();
+    expect(difficultySelector).toBeInTheDocument();
+    expect(quizButton).toBeInTheDocument();
+  });
+
+  it("selector click render test", () => {
+    const { container } = render(<Home />);
+
+    // 카테고리 드롭다운 클릭
+    fireEvent.click(categorySelector);
+    QUIZ_CATEGORY.forEach((item) => {
+      expect(container).toHaveTextContent(item.content);
+    });
+
+    // 난이도 드롭다운 클릭
+    fireEvent.click(difficultySelector);
+    QUIZ_DIFFICULTY.forEach((item) => {
+      expect(container).toHaveTextContent(item.content);
+    });
+
+    // 문제수 드롭다운 클릭
+    fireEvent.click(countSelector);
+    QUIZ_NUMBER.forEach((item) => {
+      expect(container).toHaveTextContent(item.content);
+    });
   });
 });
