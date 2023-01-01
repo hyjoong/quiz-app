aa9927a02b67cf4f076d2f35880f8550afc6cc33
기면증 <70426440+hyjoong@users.noreply.github.com>
2023-01-01 11:32:31 +0900

    feat: quiz mock file

---
+++ mocks/quiz/index.ts
@@ -0,0 +1,18 @@
+export const QUIZ_LIZT = [
+  {
+    category: "Music",
+    correct_answer: "보기1",
+    difficulty: "easy",
+    options: ["보기1", "보기2", "보기3", "보기4"],
+    question: "퀴즈 제목",
+    type: "multiple",
+  },
+  {
+    category: "Music",
+    correct_answer: "보기1",
+    difficulty: "medium",
+    options: ["보기1", "보기2", "보기3", "보기4"],
+    question: "퀴즈 제목2",
+    type: "multiple",
+  },
+];
