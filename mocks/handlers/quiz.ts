310c1e37b26209b5c6a4894b52759135f1f6b05c
기면증 <70426440+hyjoong@users.noreply.github.com>
2023-01-01 11:32:14 +0900

    feat: handler mock 파일 제거

--- mocks/handlers/quiz.ts
+++ mocks/handlers/quiz.ts
@@ -1,23 +1,5 @@
 import { rest } from "msw";
-
-const QUIZ_LIZT = [
-  {
-    category: "Music",
-    correct_answer: "88",
-    difficulty: "medium",
-    incorrect_answers: ["보기1", "보기2", "보기3"],
-    question: "퀴즈 제목",
-    type: "multiple",
-  },
-  {
-    category: "Music",
-    correct_answer: "22",
-    difficulty: "medium",
-    incorrect_answers: ["보기1", "보기2", "보기3"],
-    question: "퀴즈 제목2",
-    type: "multiple",
-  },
-];
+import { QUIZ_LIZT } from "@mocks/quiz";
 
 export const getQuiz = rest.get("/quiz", (req, res, ctx) =>
   res(ctx.json(QUIZ_LIZT))
