6190ede54022db9d2c2e1f69f4c6073f8b04fa17
기면증 <70426440+hyjoong@users.noreply.github.com>
2023-01-01 17:32:01 +0900

    test: quiz test refactoring - container -> screen

--- __tests__/quiz.test.jsx
+++ __tests__/quiz.test.jsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import Quiz from "@pages/quiz/index";
-import { render, fireEvent } from "@testing-library/react";
+import { screen, render, fireEvent, waitFor } from "@testing-library/react";
 import { QUIZ_LIZT } from "@mocks/quiz";
 import { useStore } from "@store/index";
 
@@ -20,33 +20,38 @@ beforeEach(() => {
 
 describe("quiz page render test", () => {
   it("a quiz question is displayed ", async () => {
-    const { getByText, container } = render(<Quiz />);
-    expect(container).toHaveTextContent("난이도: easy");
+    render(<Quiz />);
 
-    expect(container).toHaveTextContent("정답:");
-    expect(container).toHaveTextContent("오답:");
+    expect(screen.getByText("난이도: easy"));
+    expect(screen.findByText("정답:"));
+    expect(screen.findByText("오답:"));
 
-    const checkButton = getByText("정답 확인");
+    const checkButton = screen.getByText("정답 확인");
+    //getByText("정답 확인");
     expect(checkButton).toBeInTheDocument();
 
-    const nextButton = getByText("다음 문제");
+    const nextButton = screen.getByText("다음 문제");
     expect(nextButton).toBeInTheDocument();
   });
 
-  it("퀴즈 문항에 대한 보기는 4개가 보여진다.", () => {
-    const { container } = render(<Quiz />);
-    expect(container.getElementsByClassName("quizContent").length).toBe(4);
+  it("퀴즈 문항에 대한 보기는 4개가 보여진다.", async () => {
+    render(<Quiz />);
+    await waitFor(() => {
+      expect(screen.getAllByRole("listitem")).toHaveLength(4);
+    });
   });
 
   it("보기를 클릭하고 '정답 확인'버튼을 클릭해서 정답 여부를 확인할 수 있다.", async () => {
-    const { container, getByText } = render(<Quiz />);
-    const checkButton = getByText("정답 확인");
-    const nextButton = getByText("다음 문제");
+    render(<Quiz />);
+    const checkButton = screen.getByText("정답 확인");
+    const nextButton = screen.getByText("다음 문제");
     expect(checkButton).toBeDisabled();
     expect(nextButton).toBeDisabled();
 
+    expect(screen.queryByText("다시 풀기")).not.toBeInTheDocument();
+
     // 보기를 클릭하면 '정답확인', '다음문제' 버튼이 활성화 된다
-    const correctItem = getByText("보기1");
+    const correctItem = screen.getByText("보기1");
     fireEvent.click(correctItem);
 
     // <button>정답 확인</button> 활성화
@@ -54,7 +59,7 @@ describe("quiz page render test", () => {
 
     // 정답인 보기를 클릭한 상태에서 정답 확인 버튼을 클릭하면 '정답입니다' 메세지를 확인할 수 있다
     fireEvent.click(checkButton);
-    expect(container).toHaveTextContent("정답입니다");
+    expect(screen.getByText("정답입니다"));
 
     // <button>다음 문제</button> 활성화
     expect(nextButton).toBeEnabled();
@@ -62,10 +67,11 @@ describe("quiz page render test", () => {
     fireEvent.click(checkButton);
 
     // 오답인 보기를 클릭한 상태에서 정답 확인 버튼을 클릭하면 '오답입니다' 메세지를 확인할 수 있다
-    const inCorrectItem = getByText("보기2");
+    const inCorrectItem = screen.getByText("보기2");
     fireEvent.click(inCorrectItem);
     fireEvent.click(checkButton);
-    expect(container).toHaveTextContent("오답입니다");
+    expect(screen.getByText("오답입니다")).toBeTruthy();
+    expect(screen.getByText("다시 풀기")).toBeInTheDocument();
   });
 });
 
