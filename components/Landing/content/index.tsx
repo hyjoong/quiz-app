55f789e2161163bb64c5bd3084249995e2d07f36
henry <70426440+hyjoong@users.noreply.github.com>
2023-01-21 16:46:02 +0900

    feat: main page i18n 적용 - i18n 설치

--- components/Landing/content/index.tsx
+++ components/Landing/content/index.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 import useIntersectionObsever from "@hooks/useIntersectionObserver";
 import { ContentStyled } from "./style";
 import { Props } from "./type";
+import { useTranslation } from "next-i18next";
 
 const Contents = ({ imgSrc, detail, isRevert = false }: Props) => {
   const contentRef = useRef<HTMLDivElement>(null);
   const isViewport = useIntersectionObsever(contentRef);
 
+  const { t } = useTranslation("common");
   return (
     <ContentStyled
       isRevert={isRevert}
@@ -21,7 +23,7 @@ const Contents = ({ imgSrc, detail, isRevert = false }: Props) => {
         alt="problem"
       />
       <div className="info">
-        <h1>{detail}</h1>
+        <h1>{t(`${detail}`)}</h1>
       </div>
     </ContentStyled>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import BaseLayout from "@components/commons/BaseLayout";
 import Content from "@components/Landing/content";
 import UpperPart from "@components/Landing/upperPart";
@@ -6,21 +7,16 @@ const Home = () => {
   return (
     <BaseLayout>
       <UpperPart />
-      <Content
-        imgSrc="problem"
-        detail="카테고리와 난이도를 설정하고 퀴즈를 풀어보세요"
-      />
-      <Content
-        imgSrc="answer"
-        detail="정답 확인 버튼을 눌러 정답을 확인할 수 있습니다"
-        isRevert={true}
-      />
-      <Content
-        imgSrc="chart"
-        detail="퀴즈를 다 풀고 정답 수와 오답 수를 확인하세요"
-      />
+      <Content imgSrc="problem" detail="introduce" />
+      <Content imgSrc="answer" detail="check" isRevert={true} />
+      <Content imgSrc="chart" detail="result" />
     </BaseLayout>
   );
 };
 
 export default Home;
+export const getStaticProps = async ({ locale }: any) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["common"])),
+  },
+});
