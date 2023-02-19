96382f03e8f083af777cf23d038c87b4feeb4151
henry <70426440+hyjoong@users.noreply.github.com>
2023-02-19 16:37:33 +0900

    feat: msw api status & route 수정

--- mocks/handlers/quiz.ts
+++ mocks/handlers/quiz.ts
@@ -1,6 +1,6 @@
 import { rest } from "msw";
 import { QUIZ_LIZT } from "@mocks/quiz";
 
-export const getQuiz = rest.get("/quiz", (req, res, ctx) =>
-  res(ctx.json(QUIZ_LIZT))
+export const getQuiz = rest.get("/api/quiz", (req, res, ctx) =>
+  res(ctx.status(200), ctx.json(QUIZ_LIZT))
 );
